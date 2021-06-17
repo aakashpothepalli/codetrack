7c71339a2d72e686ba6b94bd9829e68ded8839c3
aakash9518 <p.saiaakash518@gmail.com>
2021-06-17 14:45:27 +0530

    PWA title and theme changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Codetrack`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#4285F4`,
+        theme_color: `#4285F4`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
